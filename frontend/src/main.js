4e7ccc6b21253ec94e461da7bac72856653450f6
Richie Jarvis <richard.jarvis@elastic.co>
2019-10-31 14:47:03 +0000

    Added env var for server to frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -19,7 +19,7 @@ const apm = initApm({
   // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space
   serviceName: 'frontend',
   // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'https://53946e57d55b40f28c426b717136840c.apm.europe-west1.gcp.cloud.es.io:443',
+  serverUrl: process.env.ELASTIC_APM_SERVER_URL,
   // Set service version (required for sourcemap feature)
   serviceVersion: '',
   distributedTracingOrigins: ['http://127.0.0.1:8080']
