0b317641d36b0c29ee40d905ae07ca6a7f9db79c
Dimitri Koutsos <dimitri@nephel-mbp.local>
2020-02-01 22:09:01 +0100

    todos spans

--- todos-api/todoController.js
+++ todos-api/todoController.js
@@ -1,4 +1,5 @@
 'use strict';
+const apm = require('elastic-apm-node')
 const cache = require('memory-cache');
 const {Annotation, 
     jsonEncoder: {JSON_V2}} = require('zipkin');
@@ -23,6 +24,7 @@ class TodoController {
     create (req, res) {
         // TODO: must be transactional and protected for concurrent access, but
         // the purpose of the whole example app it's enough
+        var span = apm.startSpan('creating')
         const data = this._getTodoData(req.user.username)
         const todo = {
             content: req.body.content,
@@ -32,6 +34,7 @@ class TodoController {
 
         data.lastInsertedID++
         this._setTodoData(req.user.username, data)
+        if (span) span.end()
 
         this._logOperation(OPERATION_CREATE, req.user.username, todo.id)
 
@@ -41,10 +44,13 @@ class TodoController {
     delete (req, res) {
         const data = this._getTodoData(req.user.username)
         const id = req.params.taskId
+        var span = apm.startSpan('deleting')
         delete data.items[id]
         this._setTodoData(req.user.username, data)
+        if (span) span.end()
 
         this._logOperation(OPERATION_DELETE, req.user.username, id)
+
 
         res.status(204)
         res.send()
@@ -53,16 +59,19 @@ class TodoController {
     _logOperation (opName, username, todoId) {
         this._tracer.scoped(() => {
             const traceId = this._tracer.id;
+            var span = apm.startSpan('logging')
             this._redisClient.publish(this._logChannel, JSON.stringify({
                 zipkinSpan: traceId,
                 opName: opName,
                 username: username,
                 todoId: todoId,
             }))
+           if (span) span.end()
         })
     }
 
     _getTodoData (userID) {
+        var span = apm.startSpan('getting')
         var data = cache.get(userID)
         if (data == null) {
             data = {
@@ -82,15 +91,21 @@ class TodoController {
                 },
                 lastInsertedID: 3
             }
+         if (span) span.end()
 
             this._setTodoData(userID, data)
+            this._logOperation('GET', userID, data)
         }
         return data
     }
 
     _setTodoData (userID, data) {
+        var span = apm.startSpan('setting')
         cache.put(userID, data)
+        this._logOperation('SET', userID, data)
+        if (span) span.end()
     }
 }
 
 module.exports = TodoController
+
