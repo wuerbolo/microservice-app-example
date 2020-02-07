6341d2fa83d2d33100f1c14490f11efd6a1d171b
Dimitri Koutsos <dimitri@nephel-mbp.local>
2020-02-07 17:47:49 +0100

    installing the java apm agent via self-attach API https://www.elastic.co/guide/en/apm/agent/java/current/setup-attach-api.html

--- users-api/src/main/java/com/elgris/usersapi/UsersApiApplication.java
+++ users-api/src/main/java/com/elgris/usersapi/UsersApiApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import co.elastic.apm.attach.ElasticApmAttacher;
+
 
 @SpringBootApplication
 public class UsersApiApplication {
 
 	public static void main(String[] args) {
+                ElasticApmAttacher.attach();
 		SpringApplication.run(UsersApiApplication.class, args);
 	}
 }
