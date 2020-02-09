ec7e16e3c52bfb5519ece68519095cc3551535b9
Dimitri Koutsos <nephel@users.noreply.github.com>
2020-02-09 13:17:15 +0100

    tidying up after merge

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -35,14 +35,7 @@ module.exports = {
       '/todos': {
         target: process.env.TODOS_API_ADDRESS || 'http://127.0.0.1:8082',
         secure: false
-      },
-      '/zipkin': {
-        target: process.env.ZIPKIN_URL || 'http://127.0.0.1:9411/api/v2/spans',
-        pathRewrite: {
-          '^/zipkin': ''
-        },
-        secure: false
-      },      
+      }
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
